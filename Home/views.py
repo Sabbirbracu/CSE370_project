4933ae3156b27b3b9a27e8dc67ecd8f005ff0353
looser <sabbirahmad653@gmail.com>
2024-03-22 13:06:56 +0600

    Added base.html and base.css

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def base(requests):
+    return render(requests,'home_html/base.html')
+
+
 def index(requests):
     return render(requests,'home_html/index.html')
