7e1775ebd697dd9d762da115dba797d475ffa058
looser <sabbirahmad653@gmail.com>
2024-03-30 21:29:27 +0600

    added registration functionality. Upto only redirect login page. Not complete yet.

--- Home/models.py
+++ Home/models.py
@@ -1,2 +1,27 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+class CustomUser(AbstractUser):
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    gender = models.CharField(max_length=10)
+    nid = models.CharField(max_length=20)
+    user_type = models.CharField(max_length=10, choices=[('none','none'),('rider', 'aider'),('admin', 'admin')])
+
+    # Add unique related_name arguments for groups and user_permissions
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        related_name='custom_user_groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='custom_user_permissions'
+    )
+
+    def __str__(self):
+        return self.username
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def index(requests):
@@ -8,7 +9,7 @@ def index(requests):
 
 
 
-def login(requests):
+def login_view(requests):
     page = "login"
     return render(requests, 'home_html/login.html', {'page': page})
 
@@ -20,5 +21,17 @@ def base(requests):
 
 
 def register(request):
+    if request.method == 'POST':
+        print(request.POST)
+        print("user Type:",request.POST.get('user_type'))
+        form = CustomUserCreationForm(request.POST)
+        print([i for i in form.errors])
+        print(form.errors)
+        if form.is_valid():
+            print("ok")
+            form.save()
+            return redirect('/')  # Redirect to login page after successful registration
+    else:
+        form = CustomUserCreationForm()
     page = "register"
     return render(request, 'home_html/register.html', {'page': page})
