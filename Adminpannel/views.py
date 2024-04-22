4c7ab232afe2553aee4514b433f622ce4edad026
looser <sabbirahmad653@gmail.com>
2024-04-23 02:06:10 +0600

    added admin dashboard

--- Adminpannel/views.py
+++ Adminpannel/views.py
@@ -2,11 +2,36 @@
 from django.shortcuts import render,redirect
 from Bike_Station.models import Bike_Station
 from Home.models import CustomUser
+from MakeRide.models import Ride
 from django.shortcuts import get_object_or_404
+from django.db import connection
+from django.db.models import Count
+
+
 # Create your views here.
 def admin_dashboard(request):
+    # total_stations = Bike_Station.objects.count()
+    # total_ride_users = CustomUser.objects.filter(user_type='Rider').count()
+
+    top_stations = (
+        Ride.objects.values('station_name')
+                    .annotate(total_rides=Count('ride_id'))
+                    .order_by('-total_rides')[:2]
+    )
+        
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT COUNT(*) FROM Bike_Station")
+        total_stations = cursor.fetchone()[0]
+
+        cursor.execute("SELECT COUNT(*) FROM Home_customuser WHERE user_type = 'Rider'")
+        total_ride_users = cursor.fetchone()[0]
+
+
+    ride_history = Ride.objects.values('station_name', 'ride_id', 'destination')
+
     page = "admin_dashboard"
-    return render(request,"admin_home/admin_dashboard.html",{'page':page})
+    return render(request, "admin_home/admin_dashboard.html", {'page': page, 'total_stations': total_stations, 
+                                                               'total_ride_users': total_ride_users,'top_stations': top_stations,'ride_history':ride_history})
 
 def update_station(request):
     bikestations = Bike_Station.objects.all()
